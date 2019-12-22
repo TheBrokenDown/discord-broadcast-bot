22e5ecf2a4e2a351dba4f08c2da622b1faca90c5
Delusive- <zoncck@yandex.ru>
2019-12-22 22:05:27 +0300

    Add response code check to success
    If response status code not successful the UnsuccessfulRequestException
    will be thrown. Before this commit program could do different things
    in that situation :o

--- src/main/java/space/delusive/discord/racoonsuperbot/repository/impl/YoutubeVideoRepositoryImpl.java
+++ src/main/java/space/delusive/discord/racoonsuperbot/repository/impl/YoutubeVideoRepositoryImpl.java
@@ -1,10 +1,13 @@
 package space.delusive.discord.racoonsuperbot.repository.impl;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import kong.unirest.json.JSONObject;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.val;
+import space.delusive.discord.racoonsuperbot.exception.UnsuccessfulRequestException;
 import space.delusive.discord.racoonsuperbot.repository.YoutubeVideoRepository;
 import space.delusive.discord.racoonsuperbot.repository.dto.YoutubeVideoDto;
 
@@ -17,12 +20,17 @@ public class YoutubeVideoRepositoryImpl implements YoutubeVideoRepository {
     private final String apiToken;
 
     @Override
-    public YoutubeVideoDto getLastUploadedVideo(String channelId) {
-        Object firstItem = Unirest.get(url)
+    public YoutubeVideoDto getLastUploadedVideo(String channelId) throws UnsuccessfulRequestException {
+        HttpResponse<JsonNode> response = Unirest.get(url)
                 .routeParam("channelId", channelId)
                 .routeParam("apiToken", apiToken)
-                .asJson().getBody().getObject().getJSONArray("items").get(0);
-        val jsonObject = (JSONObject) firstItem;
+                .asJson();
+        if (isNotSuccess(response)) {
+            throw new UnsuccessfulRequestException(
+                    String.format("Response status code is not success. The code is %s and the body is %s",
+                            response.getStatus(), response.getBody().toPrettyString()));
+        }
+        val jsonObject = (JSONObject) response.getBody().getObject().getJSONArray("items").get(0); // TODO: 12/22/2019 review case if there is no videos available
         val snippet = jsonObject.getJSONObject("snippet");
 
         val videoId = jsonObject.getJSONObject("id").getString("videoId");
@@ -31,5 +39,9 @@ public class YoutubeVideoRepositoryImpl implements YoutubeVideoRepository {
         val description = snippet.getString("description");
         val channelTitle = snippet.getString("channelTitle");
         return new YoutubeVideoDto(publishTime, title, description, videoId, channelTitle);
+    }
+
+    private boolean isNotSuccess(HttpResponse response) {
+        return !response.isSuccess();
     }
 }
