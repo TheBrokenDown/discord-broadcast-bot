46caf75fc7981c5fff167de17095d271e69bf448
Delusive- <zoncck@yandex.ru>
2020-05-18 23:17:14 +0300

    Change the nobody role name constant value
    ... to the more proper one :)

--- src/main/java/space/delusive/discord/broadcastbot/util/Constants.java
+++ src/main/java/space/delusive/discord/broadcastbot/util/Constants.java
@@ -1,6 +1,6 @@
 package space.delusive.discord.broadcastbot.util;
 
 public class Constants {
-    public static final String NOBODY_ROLE_NAME_IN_DB = "noone";
+    public static final String NOBODY_ROLE_NAME_IN_DB = "nobody";
     public static final String EVERYONE_ROLE_NAME_IN_DB = "everyone";
 }
